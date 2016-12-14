80424dfe7e61505fcd5fd848607aecc54bbbad88
Ken Torry <kentorry@afrodesiamedia.com>
2016-12-14 15:59:09 -0600

    Added youtube components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+// Loading Custom Components
+import _ from 'lodash'
+import YTSearch from 'youtube-api-search';
+// Search Component
+import SearchBar from './components/search-bar'
+// Video Component
+import VideoList from './components/video-list'
+import VideoListItem from './components/video-list-item'
+import VideoDetail from './components/video-detail'
+// Header Component
+import Header from './components/header'
+
+// Youtube API call 
+const API_KEY = 'AIzaSyCtZkSrLR84V-S79zSgpMzgyY-SUMO8Ogc';
+
+  
 
 class App extends Component {
-  render() {
+
+    constructor(props) {
+
+        super(props)
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        }
+        this.videoSearch('React.js')
+
+
+
+    }
+
+    videoSearch(term){
+
+        YTSearch({key: API_KEY, term: term}, (videos) =>  {
+            this.setState({
+                videos: videos,
+                selectedVideo:videos[0]
+            })
+        })
+    }
+
+    render() {
+
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 500)
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Sprint App</h2>
+        <div className="App">
+            
+            <Header />
+            <SearchBar onSearchTermChange={videoSearch}/>
+
+            <div className="container page">
+
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                        onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                        videos={this.state.videos} />
+            </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
     );
   }
 }
---
+++ src/components/header.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+    render() {
+
+        return(
+            <section className="Header">
+                <div className="container">
+
+                    <hgroup  className="logo">
+                        <h1>My Tubes <i className="fa fa-youtube-play"></i></h1>
+
+                    </hgroup>
+
+                </div>
+
+
+            </section>
+        )
+    }
+
+}
+
+export default Header
